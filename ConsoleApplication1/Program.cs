e6d1318d7c33785ca682e01cc75592aef7058844
Sebastian <jsebastianav8@gmail.com>
2017-01-14 16:47:00 -0600

    Change

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Out.Write("Seeh");
+            Console.Out.Write("Test");
         }
     }
 }
