b526dc0e1e352fa3ceba760937fe1e02a8649165
Sebastian <jsebastianav8@gmail.com>
2017-01-14 21:46:18 -0600

    Cambio de texto

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Out.Write("Test");
+            Console.Out.Write("Prueba");
         }
     }
 }
